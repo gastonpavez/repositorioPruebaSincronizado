fff2ccb9821caa153f09e60a48384cf418120f2e
Gaston <g.pavez.monares@gmail.com>
2022-07-28 20:50:39 -0400

    modificando localmente el archivo nav

---
+++ nav.php
@@ -0,0 +1,8 @@
+<nav>
+  <ol>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Quienes Somos</a></li>
+    <li><a href="#"></a>Nosotros</li>
+    <li><a href="#"></a>Contacto</li>
+  </ol>
+</nav>
