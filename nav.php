1849844baa49114d479aa9ad890fb502df074511
antonioPavez <110207726+antonioPavez@users.noreply.github.com>
2022-07-28 20:47:57 -0400

    Update nav.php

---
+++ nav.php
@@ -0,0 +1,8 @@
+<header>
+  <ol>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Quienes Somos</a></li>
+    <li><a href="#"></a>Nosotros</li>
+    <li><a href="#"></a>Contacto</li>
+  </ol>
+</header>
